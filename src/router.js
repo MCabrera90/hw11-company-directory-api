d305857e7a3514100bfcb0afbedbb70a6ff65e78
Matheo Cabrera <mcabre24@stu.southtexascollege.edu>
2024-11-12 09:10:19 -0600

    modified vite config and router.js

--- src/router.js
+++ src/router.js
@@ -9,11 +9,11 @@ import { useAuth } from './composables/useAuth'
 const { isAuthenticated } = useAuth()
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
+  { path: '/hw11-company-directory-api/', name: 'Home', component: HomePage },
+  { path: '/hw11-company-directory-api/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/hw11-company-directory-api/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/hw11-company-directory-api/login', name: 'LoginPage', component: LoginPage },
+  { path: '/hw11-company-directory-api/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
